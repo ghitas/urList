080083ca830c74e1fcbf21981ab3e369f65d9fb4
ghitas <motnguoixala2237@gmail.com>
2018-04-18 23:05:39 +0700

    save user to cookie

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, Input, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { EventService } from '../services/event.service';
 
@@ -7,40 +7,11 @@ import { EventService } from '../services/event.service';
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
-export class LoginFormComponent implements OnInit, AfterViewInit {
-  //@ViewChild('spider') canvasRef: ElementRef;
-  // a reference to the canvas element from our template
-  @ViewChild(ElementRef, {read: "spiders"}) private canvas: ElementRef;
-
-  // setting a width and height for the canvas
-  @Input() public width = 400;
-  @Input() public height = 400;
-
-  private cx: CanvasRenderingContext2D; 
-  
-  
+export class LoginFormComponent implements OnInit {
   constructor(private router: Router, private user: EventService) {
   }
-  
-  ngAfterViewInit(): void {
-    // get the context
-    const canvasEl: any = this.canvas.nativeElement;
-    this.cx = canvasEl.getContext('2d');
 
-    // set the width and height
-    canvasEl.width = this.width;
-    canvasEl.height = this.height;
-
-    // set some default properties about the line
-    this.cx.lineWidth = 3;
-    this.cx.lineCap = 'round';
-    this.cx.strokeStyle = '#000';
-    
-    // we'll implement this method to start capturing mouse events
-    //this.captureEvents(canvasEl);
-  }
   ngOnInit() {
-    
   }
 
   loginUser(e) {
