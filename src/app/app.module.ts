30db20191eec1d1c3ff3ca7212ebc08eaf068e4c
thanh <caothanhphung2237@gmail.com>
2018-06-09 13:42:33 +0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { LeftContentComponent } from './components/left-content/left-content.com
 import { RightContentComponent } from './components/right-content/right-content.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { Http, HttpModule } from '@angular/http';
-import { PlayListService } from './services/playlist.service';
 import { EventService } from './services/event.service';
 import { PlaceholderDirective } from './directives/place-holder.directive';
 import { RouterModule, Routes } from '@angular/router';
@@ -17,11 +16,12 @@ import { FooterComponent } from './footer/footer.component';
 import { AuthuguardGuard } from './authuguard.guard';
 import { AppModalComponent } from './app-modal/app-modal.component';
 import { PlaylistManagerComponent } from './playlist-manager/playlist-manager.component';
-import { NgxMyDatePickerModule } from 'ngx-mydatepicker';
 // import ngx-translate and the http loader
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { RequestOptions, XHRBackend } from '@angular/http';
+import { HttpService } from './services/http.service';
 
 const appRoutes: Routes = [
   {
@@ -52,7 +52,6 @@ export function createTranslateLoader(http: HttpClient) {
     BrowserModule,
     FormsModule,
     HttpModule,
-    NgxMyDatePickerModule.forRoot(),
     HttpClientModule,
     RouterModule.forRoot(appRoutes),
     TranslateModule.forRoot({
@@ -86,8 +85,13 @@ export function createTranslateLoader(http: HttpClient) {
 
   bootstrap: [AppComponent],
 
-  providers: [PlayListService, EventService, AuthuguardGuard],
+  providers: [EventService, AuthuguardGuard,
+    {
+      provide: Http,
+      useFactory: (backend: XHRBackend, options: RequestOptions) => {
+        return new HttpService(backend, options);
+      },
+      deps: [XHRBackend, RequestOptions]
+    }],
 })
 export class AppModule { }
-
-
